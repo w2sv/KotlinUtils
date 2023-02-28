dc600443154d80eb872f61e074b1fb266d115bb3
w2sv <zangenbergjanek@googlemail.com>
2023-02-28 10:54:42 +0100

    Add getByOrdinal

--- lib/src/main/kotlin/com/w2sv/kotlinutils/extensions/Enum.kt
+++ lib/src/main/kotlin/com/w2sv/kotlinutils/extensions/Enum.kt
@@ -3,5 +3,8 @@
 package com.w2sv.kotlinutils.extensions
 
 val Enum<*>.nonZeroOrdinal: Int
-    get() =  // TODO: come up with better name
+    get() =
         ordinal + 1
+
+inline fun <reified T: Enum<T>> getByOrdinal(ordinal: Int): T =
+    enumValues<T>()[ordinal]
