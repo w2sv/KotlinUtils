7ddb25b3e83b68eecc27f20e635cc44b14533f3f
w2sv <zangenbergjanek@googlemail.com>
2024-11-09 18:28:39 +0100

    "0.1.5-rc1"

--- kotlinutils-coroutines/src/main/kotlin/com/w2sv/kotlinutils/coroutines/flow/MutableStateFlow.kt
+++ kotlinutils-coroutines/src/main/kotlin/com/w2sv/kotlinutils/coroutines/flow/MutableStateFlow.kt
@@ -6,13 +6,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 
 /**
- * Toggles the [MutableStateFlow.value] in an atomic manner by using [update].
- */
-fun MutableStateFlow<Boolean>.toggle() {
-    update { !it }
-}
-
-/**
  * Toggles the [MutableStateFlow.value] in an atomic manner by using [update] if it is not `null`.
  */
 fun MutableStateFlow<Boolean?>.toggle() {
